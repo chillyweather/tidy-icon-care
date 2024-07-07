3010737b80aa4cb6e20bbad36f6a4857690cf4a7
Adir <slutzkis@gmail.com>
2024-07-07 15:58:56 +0300

    Update NumInput.tsx

--- src/IconFix/NumInput.tsx
+++ src/IconFix/NumInput.tsx
@@ -7,7 +7,7 @@ const NumInput = (props: any) => {
     return (
         <div className={"flex border-slate-200 border border-solid rounded p-2 gap-2 w-full"}>
             <div className={"icon p-0 pr-5 border-r border-solid border-slate-200 "+props.icon}></div>
-            <input type={"text"} value="" placeholder={props.label} className={"w-full"} />
+            <input type={"number"} value="" placeholder={props.label} className={"w-full"} />
         </div>
     )
 };
