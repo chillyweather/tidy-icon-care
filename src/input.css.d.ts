0e33ba8f8153154cf4b62a1148ff5da148ed1b15
Adir <slutzkis@gmail.com>
2024-07-09 11:59:46 +0300

    updates

--- src/input.css.d.ts
+++ src/input.css.d.ts
@@ -1,5 +1,7 @@
 declare const styles: {
+  readonly "_color_69uub_13": string;
   readonly "_divider_69uub_109": string;
+  readonly "_hexColorInput_69uub_80": string;
   readonly "_textboxColor_69uub_1": string;
   readonly "blinkAnim": string;
   readonly "button": string;
